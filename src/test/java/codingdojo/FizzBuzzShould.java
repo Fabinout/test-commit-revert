223716f2285da814d79e4e4320b62c73698edd21
fabien lamarque <fabien.lamarque@ext.cdiscount.com>
2018-11-12 13:41:59 +0100

    working

--- src/test/java/codingdojo/FizzBuzzShould.java
+++ src/test/java/codingdojo/FizzBuzzShould.java
@@ -4,52 +4,45 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-public class FizzBuzzShould
-{
+public class FizzBuzzShould {
 
     public static final String FIZZ = "Fizz";
     private static final String BUZZ = "Buzz";
 
     @Test
-    public void returnOneForOne()
-    {
-        String result  = FizzBuzz.compute(1);
-        assertEquals("1",  result );
+    public void returnOneForOne() {
+        String result = FizzBuzz.compute(1);
+        assertEquals("1", result);
     }
 
     @Test
-    public void returnTwoForTwo()
-    {
-        String result  = FizzBuzz.compute(2);
-        assertEquals("2",  result );
+    public void returnTwoForTwo() {
+        String result = FizzBuzz.compute(2);
+        assertEquals("2", result);
     }
 
     @Test
-    public void returnFizzForThree()
-    {
-        String result  = FizzBuzz.compute(3);
-        assertEquals(FIZZ,  result );
+    public void returnFizzForThree() {
+        String result = FizzBuzz.compute(3);
+        assertEquals(FIZZ, result);
     }
 
     @Test
-    public void returnFizzForSix()
-    {
-        String result  = FizzBuzz.compute(6);
-        assertEquals(FIZZ,  result );
+    public void returnFizzForSix() {
+        String result = FizzBuzz.compute(6);
+        assertEquals(FIZZ, result);
     }
 
     @Test
-    public void returnBuzzForFive()
-    {
-        String result  = FizzBuzz.compute(5);
-        assertEquals(BUZZ,  result );
+    public void returnBuzzForFive() {
+        String result = FizzBuzz.compute(5);
+        assertEquals(BUZZ, result);
     }
 
     @Test
-    public void returnBuzzForTen()
-    {
-        String result  = FizzBuzz.compute(5);
-        assertEquals(BUZZ,  result );
+    public void returnBuzzForTen() {
+        String result = FizzBuzz.compute(10);
+        assertEquals(BUZZ, result);
     }
 
 }
