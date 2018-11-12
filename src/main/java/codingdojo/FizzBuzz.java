8ae2498b1de5e4541756802d8865bd8fd2625d50
fabien lamarque <fabien.lamarque@ext.cdiscount.com>
2018-11-12 13:31:13 +0100

    pour faire passer le script

--- src/main/java/codingdojo/FizzBuzz.java
+++ src/main/java/codingdojo/FizzBuzz.java
@@ -1,7 +1,9 @@
 package codingdojo;
 
 public class FizzBuzz {
+
     public final static String FIZZ = "Fizz";
+
     public static String compute(int input) {
         if(input == 3) {
             return FIZZ;
