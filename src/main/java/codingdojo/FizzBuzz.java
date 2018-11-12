0fc9eaaaeb7ea2c9c63d9dc4a43708c7d5a6ed7a
fabien lamarque <fabien.lamarque@ext.cdiscount.com>
2018-11-12 13:20:52 +0100

    working

--- src/main/java/codingdojo/FizzBuzz.java
+++ src/main/java/codingdojo/FizzBuzz.java
@@ -1,8 +1,9 @@
 package codingdojo;
 
 public class FizzBuzz {
-
+    public final static String FIZZ = "Fizz";
     public static String compute(int input) {
+
         return String.valueOf(input);
     }
 }
