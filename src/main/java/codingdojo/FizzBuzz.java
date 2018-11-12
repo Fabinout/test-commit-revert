aec3f5236c4dea6e95ab857f6a51b28d1aa851a4
fabien lamarque <fabien.lamarque@ext.cdiscount.com>
2018-11-12 13:36:34 +0100

    working

--- src/main/java/codingdojo/FizzBuzz.java
+++ src/main/java/codingdojo/FizzBuzz.java
@@ -5,7 +5,7 @@ public class FizzBuzz {
     public final static String FIZZ = "Fizz";
 
     public static String compute(int input) {
-        if(input %3 ==0) {
+        if (input % 3 == 0) {
             return FIZZ;
         }
         return String.valueOf(input);
