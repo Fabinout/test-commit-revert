3716b97751fcc3e9cad327248bed891f668ea81f
fabien lamarque <fabien.lamarque@ext.cdiscount.com>
2018-11-12 13:10:54 +0100

    working

--- src/test/java/codingdojo/FizzBuzzShould.java
+++ src/test/java/codingdojo/FizzBuzzShould.java
@@ -7,13 +7,13 @@ import org.junit.Test;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
+public class FizzBuzzShould
 {
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
+    public void returnOneForOne()
     {
         assertTrue( true );
     }
